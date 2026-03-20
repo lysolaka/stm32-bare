b52f6821b43b4fcd9be1fa7c5209bd959e02e354
lysolaka <lysolaka@gmail.com>
2026-03-20 18:37:03 +0100

    feat: wait for accelerometer data using interrupts

--- src/main.c
+++ src/main.c
@@ -7,6 +7,42 @@
 
 uint16_t calc_timestamp(int16_t a);
 
+void EXTI0_IRQHandler() {
+  // unpend the interrupt
+  EXTI->PR = EXTI_PR_PR0;
+
+  uint8_t buf[4];
+
+  // read 4 bytes X_L, X_H, Y_L and Y_H
+  for (uint8_t i = 0; i < 4; i++) {
+    buf[i] = spi_read(0x28 + i);
+  }
+
+  // reconstruct the int16_t
+  int16_t x = (int16_t)((uint16_t)buf[0] | ((uint16_t)buf[1] << 8));
+  int16_t y = (int16_t)((uint16_t)buf[2] | ((uint16_t)buf[3] << 8));
+
+  // set the X axis PWM value in a proper direction
+  uint16_t xi = calc_timestamp(x);
+  if (x < 0) {
+    pwm_set(xi, 1);
+    pwm_set(0, 3);
+  } else {
+    pwm_set(0, 1);
+    pwm_set(xi, 3);
+  }
+
+  // same for Y
+  uint16_t yi = calc_timestamp(y);
+  if (y < 0) {
+    pwm_set(yi, 4);
+    pwm_set(0, 2);
+  } else {
+    pwm_set(0, 4);
+    pwm_set(yi, 2);
+  }
+}
+
 void main() {
   // enable FPU
   SCB->CPACR |= (0b11 << 22) | (0b11 << 20);
@@ -19,57 +55,35 @@ void main() {
   pwm_init();
   // configure SPI1 for use with the LIS3DSH sensor
   spi_init();
-  // configure DMA for SPI1
-  spi_dma_init();
 
-  // check if we are talking to the device we expect
-  if (spi_read(0x0F) != 0x3F) {
-    return;
-  }
+  // enable GPIOE peripheral clock (already enabled but doesn't hurt to do it again)
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
+  // enable the SYSCFG peripheral clock
+  RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 
-  // configure the bandwidth and enable axes
-  spi_write(0x20, 0x63);
-  // enable DRDY interrupt
-  // spi_write(0x23, 0xC8);
+  // set PE0 as pull-down input
+  GPIOE->PUPDR |= (0b10 << GPIO_PUPDR_PUPD0_Pos);
+  // select PE0 as EXTI0 input
+  SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PE;
+  // unmask EXTI0 interrupt
+  EXTI->IMR |= EXTI_IMR_MR0;
+  // trigger on rising edge
+  EXTI->RTSR |= EXTI_RTSR_TR0;
+  // set a priority
+  NVIC->IPR[EXTI0_IRQn] = (0b0110 << 4);
+  // enable EXTI line 0 interrupt in the NVIC
+  NVIC->ISER[EXTI0_IRQn >> 5] = (1 << (EXTI0_IRQn & 0x1F));
 
   // enable interrupts
   __asm__ volatile("cpsie i");
 
+  // configure the bandwidth and enable axes
+  spi_write(0x20, 0x63);
+  // enable DRDY interrupt
+  spi_write(0x23, 0xC8);
+
   for (;;) {
-    // wait for data to be ready
-    while (!(spi_read(0x27) & (1 << 3)))
-      ;
-
-    uint8_t buf[4];
-
-    // read 4 bytes X_L, X_H, Y_L and Y_H
-    for (uint8_t i = 0; i < 4; i++) {
-      buf[i] = spi_read(0x28 + i);
-    }
-
-    // reconstruct the int16_t
-    int16_t x = (int16_t)((uint16_t)buf[0] | ((uint16_t)buf[1] << 8));
-    int16_t y = (int16_t)((uint16_t)buf[2] | ((uint16_t)buf[3] << 8));
-
-    // set the X axis PWM value in a proper direction
-    uint16_t xi = calc_timestamp(x);
-    if (x < 0) {
-      pwm_set(xi, 1);
-      pwm_set(0, 3);
-    } else {
-      pwm_set(0, 1);
-      pwm_set(xi, 3);
-    }
-
-    // same for Y
-    uint16_t yi = calc_timestamp(y);
-    if (y < 0) {
-      pwm_set(yi, 4);
-      pwm_set(0, 2);
-    } else {
-      pwm_set(0, 4);
-      pwm_set(yi, 2);
-    }
+    __asm__ volatile("wfi");
   }
 }
 
